0c9662e341a73e4aa2b6fa5411010843709b7192
CTT <gustavosilva90@gmail.com>
2022-11-16 13:54:09 -0300

    Toda as atividade de varieaveis concluida

---
+++ src/ConversãoGrausFa.java
@@ -0,0 +1,11 @@
+public class ConversãoGrausFa {
+    public static void main(String[] args) {
+        double grauC = 32.5;
+        double multi = 1.8;
+        double grauF = 32.0;
+
+        double resultado = (grauC * multi) + grauF;
+        System.out.println("O valor convetido é " + resultado);
+
+    }
+}
--- src/ConverterKilômetrosMilhas.java
+++ src/ConverterKilômetrosMilhas.java
@@ -1,7 +1,7 @@
 public class ConverterKilômetrosMilhas {
     public static void main(String[] args) {
         double kilômetros = 1.5;
-        double milhas = 1.609;
+        double milhas = 1.60;
         double resultado = kilômetros / milhas;
 
         System.out.println("O valor recebido em kilômetros é " + kilômetros + "\n" + "O resultado em milhas é " + resultado);
